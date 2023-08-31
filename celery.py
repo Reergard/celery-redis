9ec4e2a8578dd01f37aa62b628c501d68c29b03d
Reergard <133778622+Reergard@users.noreply.github.com>
2023-08-31 21:40:30 +0200

    celery.py

--- celery.py
+++ celery.py
@@ -11,28 +11,29 @@ app = Celery('FanVerse')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.broker_connection_retry_on_startup = True
 
 @app.task
 def debug_task():
     print("Debug task executed")
 
 
-app.conf.beat_schedule = {
-    'send_abandoned_notifications': {
-        'task': 'catalog.tasks.send_abandoned_notification',
-        'schedule': crontab(minute='*/2'),  # Запускать каждый день в полночь (minute=0, hour=0)
-    },
+    app.conf.beat_schedule = {
+        #'send_abandoned_notification': {
+        ##    'task': 'catalog.tasks.send_abandoned_notification',
+        #    'schedule': crontab(minute='*/5'),  # Запускать каждый день в полночь (minute=0, hour=0)
+        #},
 
-    'update_book_status_and_author': {
-        'task': 'catalog.tasks.check_abandoned_books',
-        'schedule': crontab(minute='*/2'),  # Запустится каждый час
-    },
+        'update_book_status_and_author': {
+            'task': 'catalog.tasks.check_abandoned_books',
+            'schedule': crontab(minute='*/1'),  # Запустится каждый час
+        },
 
-    'simple_debug_task': {
-        'task': 'catalog.tasks.simple_debug_task',
-        'schedule': crontab(minute='*'),
-    },
-}
+        'simple_debug_task': {
+            'task': 'catalog.tasks.simple_debug_task',
+            'schedule': crontab(minute='1'),
+        },
+    }
 
 app.autodiscover_tasks()
 
